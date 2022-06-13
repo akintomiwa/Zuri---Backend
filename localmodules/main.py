248dbb42717593b1232665aceafeed22ca2afc4c
Isaac Akin-Onigbinde <aakinonigbinde@gmail.com>
2022-06-14 00:00:06 +0100

    update

--- localmodules/main.py
+++ localmodules/main.py
@@ -1,29 +1,32 @@
 import random
-
-user_action = input("Enter your choice (r, p, s): ")
-possible_actions = ["r", "p", "s"]
-if user_action not in possible_actions:
-  print("That is an invalid selection. Please try again")
-computer_action = random.choice(possible_actions)
-# print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
-# evaluate action equality first, to reduce number of cases and simplify rest of elif blocks
-if user_action == computer_action:
-    print(f"Both players selected {user_action}. It's a tie!")
-elif user_action == "r":
-    if computer_action == "s":
-        print("Rock crushes scissors! You win!")
-    else:
-        print("Paper covers rock! You lose.")
-elif user_action == "p":
-    if computer_action == "r":
-        print("Paper covers rock! You win!")
-    else:
-        print("Scissors cuts paper! You lose.")
-elif user_action == "s":
-    if computer_action == "p":
-        print("Scissors cuts paper! You win!")
-    else:
-        print("Rock crushes scissors! You lose.")
+while True:
+    user_action = input("Enter your choice (r, p, s): ")
+    possible_actions = ["r", "p", "s"]
+    if user_action not in possible_actions:
+    print("That is an invalid selection. Please try again")
+    computer_action = random.choice(possible_actions)
+    # print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+    # evaluate action equality first, to reduce number of cases and simplify rest of elif blocks
+    if user_action == computer_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "r":
+        if computer_action == "s":
+            print("Rock crushes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "p":
+        if computer_action == "r":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "s":
+        if computer_action == "p":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock crushes scissors! You lose.")
+    one_more = input("Play one more round? (y/n) ")
+    if one_more.lower != "y":
+        break
 
 
 # If the two players choose the same “character” it’s a tie, and the game repeats
